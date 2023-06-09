6000703f8503b5c59aaa9c696c3992e1dcb9bf62
sumon2807 <sharifulbd911@gmail.com>
2023-06-10 01:12:17 +0600

    jwt token is set by post method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors=require('cors');
+const jwt=require('jsonwebtoken');
 require('dotenv').config()
 const port=process.env.PORT || 5000;
 
@@ -28,6 +29,12 @@ async function run() {
 
     const userCollection=client.db('summerDB').collection('users');
 
+    // JWT token api
+    app.post('/jwt', (req, res)=>{
+      const user=req.body;
+      const token=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      res.send({token})
+    })
     // users api
     app.get('/users', async(req, res)=>{
       const result=await userCollection.find().toArray();
