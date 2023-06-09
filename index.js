983e27f8cc2a5f5ac90a572f23bd6011c9718025
sumon2807 <sharifulbd911@gmail.com>
2023-06-10 00:08:09 +0600

    user post api is created

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection=client.db('summerDB').collection('users');
+
+    // users api
+    app.post('/users', async(req, res)=>{
+      const user=req.body;
+      const result=await userCollection.insertOne(user);
+      res.send(result);
+    });
 
 
 
