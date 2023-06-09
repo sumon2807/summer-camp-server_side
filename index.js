deee16158e111fd4e9c5c06b63205ddb402a1a03
sumon2807 <sharifulbd911@gmail.com>
2023-06-10 00:41:38 +0600

    show the user who is come from client side by get methods

--- index.js
+++ index.js
@@ -29,8 +29,18 @@ async function run() {
     const userCollection=client.db('summerDB').collection('users');
 
     // users api
+    app.get('/users', async(req, res)=>{
+      const result=await userCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res)=>{
       const user=req.body;
+      const query={email: user.email};
+      const existingUser=await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exists'})
+      }
       const result=await userCollection.insertOne(user);
       res.send(result);
     });
