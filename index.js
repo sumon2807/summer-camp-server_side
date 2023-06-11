32283c9ef996ce3a5a290baaea65200c4881410b
sumon2807 <sharifulbd911@gmail.com>
2023-06-12 00:24:06 +0600

    check admin user by get api method and set verifyJWT in the api

--- index.js
+++ index.js
@@ -64,6 +64,14 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/users/admin/:email', async(req, res)=>{
+      const email=req.params.email;
+      const query={email: email};
+      const user=await userCollection.findOne(query);
+      const result={admin: user?.role ==='admin'};
+      res.send(result);
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
